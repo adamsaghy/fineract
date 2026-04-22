a0429cef0306bcecebe35a6c885fe0dd467e2bd2
EDSONZ-WASSWA <edsonwasswa54@gmail.com>
2026-04-22 12:54:33 +0300

    WEB-479 It does not allow rescheduling.

--- fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/rescheduleloan/api/RescheduleLoansApiResourceSwagger.java
+++ fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/rescheduleloan/api/RescheduleLoansApiResourceSwagger.java
@@ -217,6 +217,8 @@ final class RescheduleLoansApiResourceSwagger {
         public String locale;
         @Schema(example = "dd MMMM yyyy")
         public String dateFormat;
+        @Schema(example = "true")
+        public Boolean waiveOverdueCharges;
     }
 
     @Schema(description = "PostCreateRescheduleLoansResponse ")
